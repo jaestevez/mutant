357f4e098a0521a88ef963b45509097431496ced
Jose Estevez <jose_estevez@outlook.com>
2021-05-05 21:59:28 -0500

    fix: ajuste en propiedad de las estadisticas a almacenar

--- src/main/java/com/example/mutant/app/dao/impl/StatsDAOImpl.java
+++ src/main/java/com/example/mutant/app/dao/impl/StatsDAOImpl.java
@@ -36,7 +36,7 @@ public class StatsDAOImpl extends DataStoreInstance implements StatsDAO{
             Key taskKey = datastore.newKeyFactory().setKind("Stats").newKey("stats_check");
             Entity stat = Entity.newBuilder(taskKey)
                     .set(Constants.DS_TYPE_MUTANT, kind.equals(Constants.DS_TYPE_MUTANT) ? 1L : 0L)
-                    .set(Constants.DS_TYPE_MUTANT, kind.equals(Constants.DS_TYPE_HUMAN) ? 1L : 0L)
+                    .set(Constants.DS_TYPE_HUMAN, kind.equals(Constants.DS_TYPE_HUMAN) ? 1L : 0L)
                     .build();
             datastore.put(stat);
         } else {
